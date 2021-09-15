f26c29bfd985cefadebdbaba657f9a6f0c8d74c5
tt-stanimirchavdarov <stanimir.chavdarov@thintech.co.uk>
2021-09-15 09:40:10 +0100

    Create TPMM_Deployment_2109.ps1

---
+++ TPMM_Deployment_2109.ps1
@@ -0,0 +1,30 @@
+﻿#deploy tpmm via powershell
+
+$tpmm_exe_Url = "https://github.com/tt-stanimirchavdarov/tpmm_w10/raw/99dcd234cd1b88bad00fbc1e55f2445e25f0eeb3/TPMM.exe"
+$tpmm_exe_Target = "$env:APPDATA\TPMM.exe"
+
+$ctxhide_Url = "https://raw.githubusercontent.com/tt-stanimirchavdarov/tpmm_w10/99dcd234cd1b88bad00fbc1e55f2445e25f0eeb3/ctxhide.exe"
+$ctxhide_Target = "$env:APPDATA\ctxhide.exe"
+
+$plogon_Url = "https://raw.githubusercontent.com/tt-stanimirchavdarov/tpmm_w10/99dcd234cd1b88bad00fbc1e55f2445e25f0eeb3/Profile_Logon.cmd"
+$plogon_Target = "$env:APPDATA\Profile_Logon.cmd"
+
+$plogoff_Url = "https://raw.githubusercontent.com/tt-stanimirchavdarov/tpmm_w10/99dcd234cd1b88bad00fbc1e55f2445e25f0eeb3/Profile_Logoff.cmd"
+$plogoff_Target = "$env:APPDATA\Profile_Logoff.cmd"
+
+$pini_Url = "https://raw.githubusercontent.com/tt-stanimirchavdarov/tpmm_w10/99dcd234cd1b88bad00fbc1e55f2445e25f0eeb3/Profile_Settings_w10.INI"
+$pini_Target = "$env:APPDATA\Profile_Settings_w10.INI"
+
+Invoke-WebRequest $tpmm_exe_Url -OutFile $tpmm_exe_Target
+Invoke-WebRequest $ctxhide_Url -OutFile $ctxhide_Target
+Invoke-WebRequest $plogon_Url -OutFile $plogon_Target
+Invoke-WebRequest $plogoff_Url -OutFile $plogoff_Target
+Invoke-WebRequest $pini_Url -OutFile $pini_Target
+
+
+
+
+
+#Start-Process -FilePath $tpmm_exe_Target -Wait
+
+
