94a16a46c30c7fc26fbc3b651cabfce5583f3903
tt-stanimirchavdarov <stanimir.chavdarov@thintech.co.uk>
2021-09-15 13:14:12 +0100

    Update TPMM_Deployment_2109.ps1

--- TPMM_Deployment_2109.ps1
+++ TPMM_Deployment_2109.ps1
@@ -1,20 +1,28 @@
-﻿#deploy tpmm via powershell
+﻿#deploy tpmm via powershell - to be run as an admin from Intune
 #Stan Chavdarov - Thintech Sep 2021
 
+#prepare ThinTech folder on C:
+$path = "c:\Thintech"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+#download TPMM into the folder
 $tpmm_exe_Url = "https://github.com/tt-stanimirchavdarov/tpmm_w10/raw/99dcd234cd1b88bad00fbc1e55f2445e25f0eeb3/TPMM.exe"
-$tpmm_exe_Target = "$env:APPDATA\TPMM.exe"
+$tpmm_exe_Target = "$path\TPMM.exe"
 
 $ctxhide_Url = "https://raw.githubusercontent.com/tt-stanimirchavdarov/tpmm_w10/99dcd234cd1b88bad00fbc1e55f2445e25f0eeb3/ctxhide.exe"
-$ctxhide_Target = "$env:APPDATA\ctxhide.exe"
+$ctxhide_Target = "$path\ctxhide.exe"
 
 $plogon_Url = "https://raw.githubusercontent.com/tt-stanimirchavdarov/tpmm_w10/99dcd234cd1b88bad00fbc1e55f2445e25f0eeb3/Profile_Logon.cmd"
-$plogon_Target = "$env:APPDATA\Profile_Logon.cmd"
+$plogon_Target = "$path\Profile_Logon.cmd"
 
 $plogoff_Url = "https://raw.githubusercontent.com/tt-stanimirchavdarov/tpmm_w10/99dcd234cd1b88bad00fbc1e55f2445e25f0eeb3/Profile_Logoff.cmd"
-$plogoff_Target = "$env:APPDATA\Profile_Logoff.cmd"
+$plogoff_Target = "$path\Profile_Logoff.cmd"
 
 $pini_Url = "https://raw.githubusercontent.com/tt-stanimirchavdarov/tpmm_w10/99dcd234cd1b88bad00fbc1e55f2445e25f0eeb3/Profile_Settings_w10.INI"
-$pini_Target = "$env:APPDATA\Profile_Settings_w10.INI"
+$pini_Target = "$path\Profile_Settings_w10.INI"
 
 
 
@@ -26,14 +34,5 @@ Invoke-WebRequest $pini_Url -OutFile $pini_Target
 
 #Start-Process -FilePath $tpmm_exe_Target -Wait 
 
-
-#Prepare logoff script:
-
-$path = "$env:SystemRoot\system32\GroupPolicy\User\Scripts\Logoff"
-If(!(test-path $path))
-{
-      New-Item -ItemType Directory -Force -Path $path
-}
-
-
-Copy-Item $plogoff_Target -Destination "$env:SystemRoot\system32\GroupPolicy\User\Scripts\Logoff"
+#set the logoff script
+cmd.exe /c "schtasks /create /sc minute /mo 60 /tn TPMM_Logoff /tr $plogoff_Target"
